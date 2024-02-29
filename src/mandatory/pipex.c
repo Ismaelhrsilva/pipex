d1068de20f0d96a427eae579f578aa11a981561e
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-02-29 20:06:35 -0300

    organizando arquivos

--- src/mandatory/pipex.c
+++ src/mandatory/pipex.c
@@ -6,56 +6,11 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:24:02 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/02/29 20:01:26 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/02/29 20:05:34 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mandatory/pipex.h"
-
-static t_node	*create_cmd_node(char **args, int t)
-{
-	t_node	*node;
-
-	node = malloc(sizeof(t_node));
-	if (node == NULL)
-	{
-		perror("Node");
-		exit(1);
-	}
-	node->type = NODE_CMD;
-	node->args = args;
-	node->left = NULL;
-	node->right = NULL;
-	node->t = t;
-	return (node);
-}
-
-static t_node	*create_pipe_node(t_pipex *pipex)
-{
-	t_node	*node;
-
-	node = malloc(sizeof(t_node));
-	if (node == NULL)
-	{
-		perror("Node");
-		exit(1);
-	}
-	node->type = NODE_PIPE;
-	node->left = pipex->cmd1;
-	node->right = pipex->cmd2;
-	node->t = 0;
-	node->args = NULL;
-	return (node);
-}
-
-static void	free_ast(t_node *root)
-{
-	if (root == NULL)
-		return ;
-	free_ast(root->left);
-	free_ast(root->right);
-	free(root);
-}
 
 static void	ft_ast(t_node *root, t_pipex *pipex)
 {
