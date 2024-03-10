10d835e12b0f304a848d97ef7592c44f1f27d87a
Ismael Henrique Da Silva <ishenriq@c2r3p5.42sp.org.br>
2024-03-10 13:03:46 -0300

    começando o bonus5

--- src/bonus/utils2_bonus.c
+++ src/bonus/utils2_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:24:02 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/10 12:44:45 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/03/10 13:03:37 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ void	construct_fds(t_pipex *pipex)
 	int	i;
 
 	i = 0;
-	pipex -> fds = ft_calloc(data -> argc, sizeof(int **));
-	if (!(data -> fds))
+	pipex -> fds = ft_calloc(pipex->argc - 2, sizeof(int **));
+	if (!(pipex->fds))
 		ft_error(pipex, "fds", "Failed to allocate memory", 1);
-	while (i < pipex -> argc - 1)
+	while (i < pipex->argc - 3)
 	{
-		pipex -> fds[i] = ft_calloc(2, sizeof(int));
-		if (!(pipex -> fds[i++]))
+		pipex->fds[i] = ft_calloc(2, sizeof(int));
+		if (!(pipex->fds[i++]))
 			ft_error(pipex, "fd", "Failed to allocate memory", 1);
 	}
 }
--- src/bonus/utils_bonus.c
+++ src/bonus/utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:24:02 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/03/10 13:00:08 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/03/10 13:02:01 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ void	close_and_end(t_pipex *pipex)
 {
 	if (!pipex->type_filename && pipex->filename)
 		free(pipex->filename);
-	close(pipex->fds[ncmd]);
+	close_fds(pipex->fds[pipex->ncmd]);
 	erase(pipex);
 }
 
