1903bfa33e05f5e1289ed833588af931682d451a
Ismael Henrique Da Silva <ismaelhrsilva@gmail.com>
2024-05-01 14:56:19 -0300

    arrumei o here_doc

--- src/bonus/heredoc_bonus.c
+++ src/bonus/heredoc_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ishenriq <ishenriq@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 18:24:02 by ishenriq          #+#    #+#             */
-/*   Updated: 2024/04/21 16:10:12 by ishenriq         ###   ########.fr       */
+/*   Updated: 2024/05/01 14:55:33 by ishenriq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ char	*ft_heredoc(t_pipex *pipex)
 {
 	char	*gnl;
 	int		size;
+	int		size_gnl;
 
 	pipex->infile = open(TEMP, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (pipex->infile < 0)
@@ -24,8 +25,12 @@ char	*ft_heredoc(t_pipex *pipex)
 	{
 		ft_printf("> ");
 		gnl = get_next_line(STDIN_FILENO);
-		size = ft_strlen(gnl) - 1;
-		if (gnl && !ft_strncmp(gnl, pipex->argv[2], size))
+		if (gnl)
+			size_gnl = ft_strlen(gnl) - 1;
+		else
+			break ;
+		size = ft_strlen(pipex->argv[2]);
+		if (gnl && !ft_strncmp(gnl, pipex->argv[2], size) && size == size_gnl)
 			break ;
 		ft_putstr_fd(gnl, pipex->infile);
 		free(gnl);
